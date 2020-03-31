8afee701237d798841e2cf3737baa3904b30a582
kaveer rajcoomar <kaveer.rajcoomar@hotmail.com>
2020-03-31 16:39:56 +0400

    Add angular material in project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatCheckboxModule} from '@angular/material/checkbox';
+
 import { GenerateKeyComponent } from './generate-key/generate-key.component';
 import { CryptographyComponent } from './cryptography/cryptography.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     GenerateKeyComponent,
-    CryptographyComponent
+    CryptographyComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
