b1a28af3ee304999e1491a58004d50bc96db44f1
kaveer rajcoomar <kaveer.rajcoomar@hotmail.com>
2020-04-01 01:03:39 +0400

    Routing for about us

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {GenerateKeyComponent} from "./generate-key/generate-key.component";
 import {CryptographyComponent} from "./cryptography/cryptography.component";
+import {AboutUsComponent} from "./about-us/about-us.component"
 
 
 const routes: Routes = [
   {path: '', component : GenerateKeyComponent},
-  {path: 'crypt', component : CryptographyComponent}
+  {path: 'crypt', component : CryptographyComponent},
+  {path: 'about', component: AboutUsComponent}
 ];
 
 @NgModule({
