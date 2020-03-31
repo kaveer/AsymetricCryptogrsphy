685fe68f8f8b95f6ee03c8ea45936c0997e83796
kaveer rajcoomar <kaveer.rajcoomar@hotmail.com>
2020-03-31 15:17:03 +0400

    Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {GenerateKeyComponent} from "./generate-key/generate-key.component";
+import {CryptographyComponent} from "./cryptography/cryptography.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component : GenerateKeyComponent},
+  {path: 'crypt', component : CryptographyComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
